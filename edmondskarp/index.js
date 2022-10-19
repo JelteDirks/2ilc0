c4d59a818ae859afbb6d57aa52d3f10a4845f7f8
Jelte Dirks <j.dirks@outlook.com>
2022-10-19 12:56:38 +0200

    data structure and finding autmenting path
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

---
+++ edmondskarp/index.js
@@ -0,0 +1,57 @@
+function find_augmenting_path(G, s, t) {
+  let Q = [[s]]
+  while (Q.length > 0) {
+    let next = Q.shift();
+    let last = next[next.length - 1]
+    console.log("last",last);
+    if (last === t) {
+      console.log("path", next);
+      break;
+    }
+    for (let i = 0 ; i < G[last].length; i++) {
+      if (G[last][i] < Infinity) {
+        Q.push([...next, i]);
+      }
+    }
+  }
+}
+
+let G = []
+
+for (let i = 0 ; i < 11; i++) {
+  if (!G[i]) {
+    G.push([])
+  }
+  for (let j = 0; j < 11; j++) {
+    G[i][j] = Infinity
+  }
+}
+
+let edges = [
+  [0,1],
+  [1,2],
+  [1,3],
+  [0,4],
+  [4,2],
+  [4,6],
+  [0,5],
+  [5,6],
+  [0,7],
+  [7,8],
+  [0,9],
+  [9,8],
+  [2,10],
+  [3,10],
+  [6,10],
+  [8,10],
+]
+
+for (let [u,v] of edges) {
+  G[u][v] = 1
+}
+
+function key(u,v) {
+  return "u=" + u + " v=" + v;
+}
+
+find_augmenting_path(G, 0, 10);
